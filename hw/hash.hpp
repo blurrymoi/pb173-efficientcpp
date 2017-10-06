1ecc1aa338569be6b3b4566f56feac1028d61493
blurry <blurry@localhost.localdomain>
2017-10-06 22:45:13 +0200

    hw01: Make linked-list kind of functional

--- hw/hash.hpp
+++ hw/hash.hpp
@@ -41,37 +41,64 @@ public:
 
     void insert( T item )
     {
-        cout << "inserting item " << item << endl;
+        //cout << "inserting item " << item << endl;
         auto h = std::make_unique< H<Key,T> >( item );
-        cout << "h.item " << h->item << endl;
+        insert( std::move( h ) );
+    }
 
-        insert( h );
+    //not fetching item directly so that i can return nullptr
+    H<Key,T>* find ( Key hash )
+    {
+        H<Key,T>* ret;
+        if (!items[hash % size]) return nullptr;
+        else ret = items[hash % size].get();
+        do {
+                if ( ret && ret->hash == hash )
+                    return ret;
+                if ( !ret->next ) 
+                    return nullptr;
+                ret = ret->next.get();
+            } while(1);
     }
 private:
-    void insert( std::unique_ptr< H<Key,T> >& h )
+    void insert( std::unique_ptr< H<Key,T> > h )
     {
-        //place free        
-        if ( !items[h->hash % size] ) {
-            items[h->hash % size] = std::move(h);
+        T val = h->item;//DELETE
+        size_t index = h->hash % size;
+        if ( !items[index] ) {
+            //cout << "wasempty" << endl;
+            items[index] = std::move( h );
         } else {
-            H< Key,T >* place = items[h->hash % size].get();
-            while( place->next ) {
-                if ( place->item == h->item ) { //"set"
-                    cout << "!items match" << endl;
+            //cout << "wasnotempty" << endl;
+            H< Key,T >* place = items[index].get();
+            
+            do {
+                if ( place && place->item == h->item ) {  
+                    //cout << "!items match" << endl;
                     return;
                 }
+                if ( !place->next ) break;
                 place = place->next.get();
-            }
+            } while(1);
+
             place->next = std::move(h);
         }
 
-        cout << "inserted: item - " << h->item << endl;       
+        //cout << "inserted: item - " << val << endl;       
         num_items++;
 
         //if (ratio.bad) rehash()
-        if ( (num_items/size) > (2/3) ) {
+        if ( ((float)num_items/size) > ((float)3/5) ) {
+
+            /*
+            cout << "** rehashing **" << endl;
+            cout << "** num_items => " << num_items << endl;
+            cout << "** size => " << size << endl;
+            */
+            
             size *= 2;
             items.resize( size );
+            //printhashy();
             rehash();
         }
     }
@@ -86,15 +113,53 @@ private:
                 stuff_that_needs_rehashing.push( move(i) );
             }
         }
+        //cout << "maybe stacked" << endl;
+        //printhashy();
 
+        num_items = 0;
         while ( !stuff_that_needs_rehashing.empty() ) {
             std::unique_ptr< H<Key,T> > item = std::move( stuff_that_needs_rehashing.top() ); 
-                       stuff_that_needs_rehashing.pop();
+            stuff_that_needs_rehashing.pop();
+
+            std::unique_ptr< H<Key,T> > next = std::move( item );
+            std::unique_ptr< H<Key,T> > nextnext;
+
             do {
-                insert( item );
-                std::unique_ptr< H<Key,T> > item = std::move(item->next);
+                if (next)
+                    nextnext = std::move(next->next);
+                insert( std::move( next ) );
+
+                //cout << "post INSERT in REHASH" << endl;
+                //printhashy();
+
+                if ( !nextnext )
+                    break;
+                else
+                    next = std::move( nextnext );
                 //items.insert(item);
-            } while (item);         
+            } while (1);         
         }
+    }
+
+public:
+    void printhashy()
+    {
+        cout << "##########" << endl;
+        for(size_t i = 0; i < size; ++i) {
+            cout << i << ": " << endl;            
+            if (!items[i])
+                cout << "NULL" << endl;
+            else {
+                auto h = items[i].get();
+                while (h) {
+                    cout << "item: " << h->item << endl;
+                    cout << "hash: " << h->hash << endl;
+                    h = h->next.get();
+                }
+            }
+        }
+        cout << "num items :: " << num_items << endl;
+        cout << "##########" << endl;
+            
     }
 };
--- hw/mainhash.cpp
+++ hw/mainhash.cpp
@@ -2,6 +2,9 @@
 #include <iostream>
 #include <string>
 #include <assert.h>
+#include <time.h>
+#include <random>
+#include <climits>
 
 using namespace std;
 
@@ -10,13 +13,35 @@ int main()
     HashLinked<int, int> h;
 
     h.insert(6);
+    //h.printhashy();
+
     h.insert(4);
+    //h.printhashy();
+
     h.insert(-2);
     h.insert(22);
     
     assert(h.num_items == 4);
     h.insert(4);
     assert(h.num_items == 4);
+    //h.printhashy();
+
+    assert( h.find(6)->item == 6 );
+    assert( h.find(5) == nullptr );
+ 
+    std::default_random_engine gen{static_cast<unsigned long>(time(NULL))};
+    std::uniform_int_distribution<> distr(INT_MIN, INT_MAX);
+
+    HashLinked<int,int> hl;
+    clock_t vecq_beg = clock();    
+
+    for( unsigned i = 0; i < 10000000; ++i )
+        hl.insert( distr(gen) );
+    //while(!qu.empty())
+    //    qu.remove();
+
+    clock_t vecq_end = clock();
+    cout << "LL: " << vecq_end - vecq_beg << endl;
 
     return 0;
 }
